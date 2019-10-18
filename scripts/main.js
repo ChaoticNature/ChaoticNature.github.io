fc338693fa80ffd02397f4ff6210877bb4f1e78d
ChaoticNature <ghsfer@gmail>
2019-10-18 03:48:36 -0300

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+var miBoton = document.querySelector('button');
+var miTitulo = document.querySelector( 'h1');
+function estableceNombreUsuario() {
+    var miNombre = prompt('Por favor, ingresa tu nombre.');
+    localStorage.setItem('nombre', miNombre);
+    miTitulo.textContent = 'Hablemos de Waifus,' + miNombre;
+}
+if (!localStorage.getItem('nombre')) {
+    estableceNombreUsuario();
+}
+else {
+    var nombreAlmacenado = localStorage.getItem('nombre');
+    miTitulo.textContent = 'Hablemos de Waifus,' + nombreAlmacenado;
+}
+miBoton.onclick = function() {
+    estableceNombreUsuario();
+}
